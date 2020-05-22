302abd2bdf8c18b980b1a55e61255e5ff5d0ab92
J. Matthew Griffis <matticgreek@gmail.com>
2020-05-22 10:22:11 -0600

    Implement `OnInit` and move the subscription logic there.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'node-azure-servicebus-explorer-fe';
   public messages = null;
 
   private apiUrl = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
+
+  public ngOnInit(): void {
     this.getTopics().subscribe((msgs) => {
       this.messages = msgs;
     });
