b53e50ca339db192cfec190990f17f4ce7db1a37
J. Matthew Griffis <matticgreek@gmail.com>
2020-05-22 10:29:38 -0600

    Implement `OnDestroy`, subscription management, and emission handler.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,26 +1,34 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnDestroy, OnInit {
   public title = 'node-azure-servicebus-explorer-fe';
   public messages = null;
 
+  private subscriptions = new Subscription();
   private apiUrl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) {}
 
   public ngOnInit(): void {
-    this.getTopics().subscribe((msgs) => {
-      this.messages = msgs;
-    });
+    this.subscriptions.add(this.getTopics().subscribe(this.handleMessages));
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   private getTopics() {
     return this.http.get(this.apiUrl + '/peek');
   }
+
+  private handleMessages = (msgs: any[] = []): void => {
+    this.messages = msgs;
+  };
 }
