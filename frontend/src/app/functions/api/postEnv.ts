8e3698e7630d9e2de58986ce243ab6d07fbd1977
J. Matthew Griffis <matticgreek@gmail.com>
2020-06-01 15:50:24 -0600

    Add error handling to `postEnv`.

--- frontend/src/app/functions/api/postEnv.ts
+++ frontend/src/app/functions/api/postEnv.ts
@@ -1,15 +1,20 @@
-import { Observable } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, flatMap } from 'rxjs/operators';
 import { PostRequestBody } from '../../structs';
 import { ApiConfig } from './apiConfig';
 import { ApiRoutes } from './apiRoutes';
 import { killServer } from './killServer';
+import { noteError } from './noteError';
 
 interface PostConfig extends ApiConfig {
   env: PostRequestBody;
 }
 
 export const postEnv = ({ http, url, env }: PostConfig): Observable<any> =>
-  http
-    .post(`${url}/${ApiRoutes.setEnv}`, env)
-    .pipe(flatMap((response: any) => killServer({ http, url })));
+  http.post(`${url}/${ApiRoutes.setEnv}`, env).pipe(
+    catchError((error: any) => {
+      noteError('update environment', error);
+      return of();
+    }),
+    flatMap((response: any) => killServer({ http, url }))
+  );
