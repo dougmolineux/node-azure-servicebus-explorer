dc834a5115b87c1e08d09e8b2fd85eff8cf7cd2d
J. Matthew Griffis <matticgreek@gmail.com>
2020-06-01 15:58:02 -0600

    Add error handling to `killServer`.

--- frontend/src/app/functions/api/killServer.ts
+++ frontend/src/app/functions/api/killServer.ts
@@ -1,6 +1,13 @@
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { ApiConfig } from './apiConfig';
 import { ApiRoutes } from './apiRoutes';
+import { noteError } from './noteError';
 
 export const killServer = ({ http, url }: ApiConfig): Observable<any> =>
-  http.post(`${url}/${ApiRoutes.kill}`, null);
+  http.post(`${url}/${ApiRoutes.kill}`, null).pipe(
+    catchError((error: any) => {
+      noteError('restart server', error);
+      return of();
+    })
+  );
