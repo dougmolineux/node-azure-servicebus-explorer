3cbca8dfe891cec6f9ed258fe34f1255c1ee0b68
J. Matthew Griffis <matticgreek@gmail.com>
2020-06-01 16:29:35 -0600

    Display messages that are objects.

--- frontend/src/app/functions/processMessages.ts
+++ frontend/src/app/functions/processMessages.ts
@@ -1,2 +1,11 @@
+const processMessage = (x: any): any => {
+  const body: any = x?.[0]?.body ?? null;
+  return body === null
+    ? []
+    : typeof body === 'string'
+    ? body
+    : JSON.stringify(body);
+};
+
 export const processMessages = (messages: any[] = []): string[] =>
-  messages?.flatMap((x: any): string => x?.[0]?.body ?? []) ?? [];
+  messages?.flatMap(processMessage) ?? [];
