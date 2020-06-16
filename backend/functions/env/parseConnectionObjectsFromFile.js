75711e1fe55e3956db9a4b082b1f07aaa614d6ba
J. Matthew Griffis <matticgreek@gmail.com>
2020-06-16 15:50:13 -0600

    Update parseConnectionObjectsFromFile.js
    Handle empty file contents (which may be the result of trying to read a nonexistent file).

--- backend/functions/env/parseConnectionObjectsFromFile.js
+++ backend/functions/env/parseConnectionObjectsFromFile.js
@@ -1,16 +1,22 @@
+const isPopulated = require('../isPopulated');
 const readFile = require('../readFile');
 const {
   delimiters: { external: delimiter },
 } = require('./constants');
 const parseConnectionObjectFromString = require('./parseConnectionObjectFromString');
 
+const processFileContents = (fileContents = '') =>
+  isPopulated(fileContents)
+    ? fileContents
+        .split(delimiter)
+        .map(parseConnectionObjectFromString)
+        .filter(Boolean)
+    : [];
+
 const parseConnectionObjectsFromFile = async (file) => {
   try {
     const fileContents = await readFile(file);
-    return fileContents
-      .split(delimiter)
-      .map(parseConnectionObjectFromString)
-      .filter(Boolean);
+    return processFileContents(fileContents);
   } catch (error) {
     console.log(
       'Failed to parse connection objects from file with error:',
